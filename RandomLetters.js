0b6825992fe6ca9f21d0b13f6c085fa8763c461b
eladjmc <eladjmc88@gmail.com>
2023-01-23 18:48:28 +0200

    added error handling

--- RandomLetters.js
+++ RandomLetters.js
@@ -1,16 +1,30 @@
-const uppercase = document.getElementById('uppercase');
-const lowercase = document.getElementById('lowercase');
-const numbers = document.getElementById('numbers');
-const symbols = document.getElementById('quantity');
-const lengthOfPass = document.getElementById('length');
-const numberOfPasswords = document.getElementById('pass-amount');
-const passwordBox = document.getElementById("pass-box")
+const uppercase = document.getElementById("uppercase");
+const lowercase = document.getElementById("lowercase");
+const numbers = document.getElementById("numbers");
+const symbols = document.getElementById("quantity");
+const lengthOfPass = document.getElementById("length");
+const numberOfPasswords = document.getElementById("pass-amount");
+const passwordBox = document.getElementById("pass-box");
 
+const minMost = 1;
+const minLen = 8;
+const maxMost = 5;
+const maxLen = 12;
 
-
-
-
-
+const isError = (arr, minMaxL, amountOfPass) => {
+  if (amountOfPass < 1 || amountOfPass > 3) {
+    return true;
+  }
+  if (minMaxL < minLen || minMaxL > maxLen) {
+    return true;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < minMost || arr[i] > maxMost) {
+      return true;
+    }
+    return false;
+  }
+};
 function randomLetter(capital, lower, numbers, symbol) {
   let letters = "";
 
@@ -26,74 +40,76 @@ function randomLetter(capital, lower, numbers, symbol) {
   if (capital > 0) {
     for (let i = 0; i < capital; i++) {
       letters += upperCaseArr[Math.floor(Math.random() * upperCaseArr.length)];
-
     }
   }
   if (lower > 0) {
     for (let i = 0; i < lower; i++) {
       letters += lowerCaseArr[Math.floor(Math.random() * lowerCaseArr.length)];
-
     }
   }
   if (numbers > 0) {
     for (let i = 0; i < numbers; i++) {
       letters += numbersArr[Math.floor(Math.random() * numbersArr.length)];
-
     }
   }
   if (symbol > 0) {
     for (let i = 0; i < symbol; i++) {
       letters += symbolsArr[Math.floor(Math.random() * symbolsArr.length)];
-
     }
   }
-
 
   return letters;
 }
 
-
-
 const generateExtraChars = (length) => {
-  let result           = '';
-  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*';
+  let result = "";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*";
   let charactersLength = characters.length;
-  for ( let i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
-}
-
+};
 
 const mixString = (str) => {
- str = str.split('').sort(() => {
-  return Math.floor(Math.random() * (5 - (-5)) + 5) -10;
- }).join('');
- return str; 
-}
-
+  str = str
+    .split("")
+    .sort(() => {
+      return Math.floor(Math.random() * (5 - -5) + 5) - 10;
+    })
+    .join("");
+  return str;
+};
 
 function generatorPassword() {
   let e = document.querySelectorAll("p");
-  if(e){
-    e.forEach(element => {
+  if (e) {
+    e.forEach((element) => {
       element.remove();
     });
   }
-  
+
   const upper = Number(uppercase.value);
   const lower = Number(lowercase.value);
   const nums = Number(numbers.value);
   const symb = Number(symbols.value);
   const len = Number(lengthOfPass.value);
   const numOfPasswords = Number(numberOfPasswords.value);
-  for (let i =0 ;i < numOfPasswords ;i++ ){
-  let pass = randomLetter(upper, lower, nums, symb);
-  pass += generateExtraChars(len - pass.length);
-  pass = mixString(pass);
-  const newP = document.createElement("p");
-  newP.textContent = pass;
-  passwordBox.appendChild(newP);
-}
-}
 
+  if (isError([upper, lower, nums, symb], len, numOfPasswords)) {
+    const errorP = document.createElement("p");
+    errorP.textContent = 'Error! bad input';
+    passwordBox.appendChild(errorP);
+    return;
+  }
+
+  for (let i = 0; i < numOfPasswords; i++) {
+    let pass = randomLetter(upper, lower, nums, symb);
+    pass += generateExtraChars(len - pass.length);
+    pass = mixString(pass);
+    const newP = document.createElement("p");
+    newP.textContent = pass;
+    passwordBox.appendChild(newP);
+  }
+}
